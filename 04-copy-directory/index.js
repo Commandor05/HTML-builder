831d95f8dbf02944764e815c329003dc4daed7c8
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2022-11-06 22:41:09 +0200

    feat: add solution for folder copying task(4)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,49 @@
+const path = require('path');
+const { mkdir, readdir, copyFile, unlink } = require('fs');
 
+const sourceDir = 'files';
+const destinationDir = 'files-copy';
+const sourcePath = path.join(__dirname, sourceDir);
+const destinationPath = path.join(__dirname, destinationDir);
+
+function clearDestinationDir() {
+  readdir(destinationPath,
+    { withFileTypes: true },
+    (error, files) => {
+      if (error) return console.log(error.message);
+      files.forEach(file => {
+        const filePath = path.join(destinationPath, file.name);  
+        unlink(filePath, (error) => {
+          if (error) return console.log(error.message);
+        });         
+      });
+  });
+}
+
+function copyDir() {
+  mkdir(destinationPath,
+    { recursive: true },
+    (error) => {
+      if (error) return console.error(error.message);
+      console.log('Directory created');
+      clearDestinationDir();
+
+      readdir(sourcePath,
+        { withFileTypes: true },
+        (error, files) => {
+          if (error) return console.log(error.message);
+          files.forEach(file => {
+            const fileSourcePath = path.join(sourcePath, file.name);  
+            const fileDestinationPath = path.join(destinationPath, file.name);  
+    
+            copyFile(fileSourcePath, fileDestinationPath, (err) => {
+              if (err) return console.log(error.message);            
+              console.log(`${file.name} - copied`);
+            });
+          });
+      });
+    }
+  );
+}
+
+copyDir();
